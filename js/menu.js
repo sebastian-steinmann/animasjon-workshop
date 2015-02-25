d657d09c200d3b59b40b14702b7aa011d6b994f5
Sebastian Steinmann <sebastian.steinmann@bekk.no>
2015-02-25 07:25:51 +0100

    Fixed some css bugs, added some aria improvements

--- js/menu.js
+++ js/menu.js
@@ -9,9 +9,37 @@ var MenuView = (function(window, $) {
 	View.prototype = {
 		onOpen: function(event) {
 			event.preventDefault();
-
+			console.log(this.getHeight());
 			this.el.toggleClass("is-open");
 			this.triggerEl.toggleClass("is-active");
+			this.toggleExpanded();
+		},
+		toggleExpanded: function() {
+			var expanded = this.el.attr("aria-expanded");
+			if (expanded === "true") {
+				this.el.attr("aria-expanded", "false");
+			} else {
+				this.el.attr("aria-expanded", "true");
+				this.el.focus();
+			}
+		},
+		getHeight: function() {
+			this.el.css({
+				visibility: "hidden",
+				display: "block",
+				height: "auto",
+				position: "absolute"
+			});
+
+			var h = this.el.height();
+
+			this.el.css({
+				visibility: "",
+				display: "",
+				height: "",
+				position: ""
+			});
+			return h;
 		}
 	};
 	return View;
